cd1bfd3a515eb0482f726dd3f81cf8b9695fa938
Adrian Bury <adrian.jakub.bury@gmail.com>
2022-02-09 18:07:11 +0100

    Fix label class

--- src/app/errors/required.component.ts
+++ src/app/errors/required.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-required',
-  template: `<label [for]="for" class="invalid-feedback d-block">To pole jest wymagane</label>`,
+  template: ` <label [for]="for" class="invalid-feedback d-block"> To pole jest wymagane </label> `,
 })
 export class RequiredComponent {
   @Input() for?: string;
