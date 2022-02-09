3dad5348afa3d47622743008f6ba19f1587d4e54
Adrian Bury <adrian.jakub.bury@gmail.com>
2022-02-09 18:08:49 +0100

    Fix label class

--- src/app/errors/minlength.component.ts
+++ src/app/errors/minlength.component.ts
@@ -2,7 +2,11 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-minlength',
-  template: `<label [for]="for">To pole musi składać się minimum z {{ error.requiredLength }} znaków</label>`,
+  template: `
+    <label [for]="for" class="invalid-feedback d-block">
+      To pole musi składać się minimum z {{ error.requiredLength }} znaków
+    </label>
+  `,
 })
 export class MinlengthComponent {
   @Input() for?: string;
