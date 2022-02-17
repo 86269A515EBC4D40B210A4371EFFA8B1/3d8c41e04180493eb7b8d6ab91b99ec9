e118002cf55cf3995a2900943e7ca687ad83bf30
Adrian Bury <adrian.jakub.bury@gmail.com>
2022-02-17 19:48:37 +0100

    Add repeatedPassword field with validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,33 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  form = new FormGroup({
-    email: new FormControl(null, [
-      Validators.required,
-      Validators.minLength(3),
-      Validators.maxLength(10),
-      Validators.email,
-    ]),
-    password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
-  });
+  form = new FormGroup(
+    {
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(10),
+        Validators.email,
+      ]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+      repeatedPassword: new FormControl(null),
+    },
+    {
+      validators: [passwordMatchValidator],
+    },
+  );
+}
+
+function passwordMatchValidator(control: AbstractControl): ValidationErrors | null {
+  const password = control.get('password')!;
+  const repeatedPassword = control.get('repeatedPassword')!;
+  if (password.value !== repeatedPassword.value) {
+    return { passwordMatch: true };
+  }
+  return null;
 }
